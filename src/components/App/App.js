727a3baf51d98e6fc52ff023d4e2e1f9e9f6b1ea
David Friday <davidsfriday13@gmail.com>
2019-09-10 00:02:24 -0500

    start up page working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Board from '../board/board';
 
 function App() {
   return (
     <div className="App">
+    <Board />
       
     </div>
   );
---
+++ src/components/board/board.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './board.css'
+
+function board() {
+  return (
+    <div id="board">
+
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+      
+    </div>
+  );
+}
+
+export default board;
